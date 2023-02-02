e1d20ca7c13b775e804691b8d43f8285756701de
nhasan <nazmulhasan02901@gmail.com>
2023-02-02 12:41:13 +0600

    event page horizontal scroll bar fixed

--- src/components/user/services/AllEvents.js
+++ src/components/user/services/AllEvents.js
@@ -7,34 +7,33 @@ import { setLoader, clearLoader } from "../../../store/actions/loader";
 import ConfirmationModal from "../../../helpers/confirmationModal";
 import { useInterval } from "../../../helpers/useInterval";
 import { POLLING_INTERVAL } from "../../../constants/variables";
-import CommunityImg from '../../../assets/Icons/community-full.svg'
-import EventCardImg from '../../../assets/event-card.png'
-import ShareIcon from '../../../assets/Icons/share.svg'
+import CommunityImg from "../../../assets/Icons/community-full.svg";
+import EventCardImg from "../../../assets/event-card.png";
+import ShareIcon from "../../../assets/Icons/share.svg";
 import ShareEventModal from "../../../helpers/shareEventModal";
-
 
 const tempeventList = [
   {
     id: 0,
-    name: 'All',
-    selected: true
+    name: "All",
+    selected: true,
   },
   {
     id: 1,
-    name: 'Workshops/Trainings',
-    selected: false
+    name: "Workshops/Trainings",
+    selected: false,
   },
   {
     id: 2,
-    name: 'Awards/Competitions',
-    selected: false
+    name: "Awards/Competitions",
+    selected: false,
   },
   {
     id: 3,
-    name: 'Exhibitions/Summits',
-    selected: false
+    name: "Exhibitions/Summits",
+    selected: false,
   },
-]
+];
 
 const AllEvents = (props) => {
   const dispatch = useDispatch();
@@ -43,8 +42,8 @@ const AllEvents = (props) => {
   const [selectedEvent, setSelectedEvent] = useState({});
   const [modalShow, setModalShow] = React.useState(false);
   const [modalShowDelete, setModalShowDelete] = useState(false);
-  const [eventList, setEventList] = useState(tempeventList)
-  const [shareModalActive, setShareModalActive] = useState(false)
+  const [eventList, setEventList] = useState(tempeventList);
+  const [shareModalActive, setShareModalActive] = useState(false);
 
   useInterval(async () => {
     // dispatch(setLoader());
@@ -55,129 +54,140 @@ const AllEvents = (props) => {
   }, POLLING_INTERVAL);
 
   useEffect(() => {
-    const event = eventList.find(event => event.id === parseInt(id))
-    if (!event) return
-    let tempevent = eventList.map(ev => {
+    const event = eventList.find((event) => event.id === parseInt(id));
+    if (!event) return;
+    let tempevent = eventList.map((ev) => {
       if (ev.id === parseInt(id)) {
-        return { ...ev, selected: true }
+        return { ...ev, selected: true };
       } else {
-        return { ...ev, selected: false }
+        return { ...ev, selected: false };
       }
-    })
-    setEventList(tempevent)
+    });
+    setEventList(tempevent);
   }, [id]); // eslint-disable-line react-hooks/exhaustive-deps
 
   useEffect(() => {
     dispatch(setLoader());
-    const selectedEvent = eventList.find(event => event.selected === true)
-    if (!selectedEvent) return
-    const id = selectedEvent.id
+    const selectedEvent = eventList.find((event) => event.selected === true);
+    if (!selectedEvent) return;
+    const id = selectedEvent.id;
     if (id === 0) {
       AdminService.fetchAllEvents(1).then((res) => {
         dispatch(clearLoader());
-        console.log('data', res.data.eventBeans);
+        console.log("data", res.data.eventBeans);
         setEvents(res.data.eventBeans);
       });
     } else {
       AdminService.fetchEventsByCategory(id, 1).then((res) => {
         dispatch(clearLoader());
-        console.log('data', res.data.eventBeans);
+        console.log("data", res.data.eventBeans);
         setEvents(res.data.eventBeans);
       });
     }
-  }, [eventList])
+  }, [eventList]);
 
-  const handlEventChange = id => {
-    let tempevent = eventList.map(ev => {
+  const handlEventChange = (id) => {
+    let tempevent = eventList.map((ev) => {
       if (ev.id === id) {
-        return { ...ev, selected: true }
+        return { ...ev, selected: true };
       } else {
-        return { ...ev, selected: false }
+        return { ...ev, selected: false };
       }
-    })
-    setEventList(tempevent)
-  }
+    });
+    setEventList(tempevent);
+  };
   const getEventName = () => {
-    if (id === '1') return 'Workshops & Trainings'
-    if (id === '2') return 'Awards & Competitions'
-    if (id === '3') return 'Exhibitions & Summits'
-    return ''
-  }
+    if (id === "1") return "Workshops & Trainings";
+    if (id === "2") return "Awards & Competitions";
+    if (id === "3") return "Exhibitions & Summits";
+    return "";
+  };
   return (
     <>
       <div className="pt-0 md:pt-10">
         <div className="grid grid-cols-12 md:grid-cols-12 mb-6 md:mb-10 hidden md:grid">
           <div className="col-span-12 md:col-span-8 mb-5 md:mb-0">
-            <img src={CommunityImg} className='w-full' />
+            <img src={CommunityImg} className="w-full" />
           </div>
           <div className="col-span-12 md:col-span-4 all-events-form">
             <div className="px-4 py-4 flex flex-col justify-center all-events-form-wrapper md:ml-8">
               <p className="mb-4  text-base">Post Your Event</p>
               <input className="flex-1 mb-4" />
-              <textarea className="flex-1 mb-4">
-
-              </textarea>
+              <textarea className="flex-1 mb-4"></textarea>
               <button className="w-full bg-[#0058A9] text-white">
                 Post Event Free
               </button>
             </div>
           </div>
         </div>
-        <div className="list-group row ml-0">
+        <div className="list-group row ml-0 w-full">
           <div className="all-events-categories flex items-center mb-6 md:mb-8">
-            {eventList.map(event => {
-              return <div key={event.id}
-                className={`category-item ${event.selected ? 'selected font-bold' : 'font-semibold'}`}
-                onClick={() => handlEventChange(event.id)} >
-                <p> {event.name} </p>
-              </div>
+            {eventList.map((event) => {
+              return (
+                <div
+                  key={event.id}
+                  className={`category-item ${
+                    event.selected ? "selected font-bold" : "font-semibold"
+                  }`}
+                  onClick={() => handlEventChange(event.id)}
+                >
+                  <p> {event.name} </p>
+                </div>
+              );
             })}
           </div>
           {/* <h3 className="mt-4 mb-4"> {getEventName()} </h3> */}
-          <div className='event-cards grid grid-cols-12 gap-y-4 md:gap-x-5 md:gap-x-5'>
+          <div className="event-cards grid grid-cols-12 gap-y-4 md:gap-x-5 md:gap-x-5">
             {events.map((event, index) => {
               return (
-
-                <div key={event.id} className='event-card col-span-12 md:col-span-4'
-                  onClick={() => window.open(`${process.env.REACT_APP_URL}/event/${event.eventCategoryText}/${event.id}/${event.eventId}`)}>
+                <div
+                  key={event.id}
+                  className="event-card col-span-12 md:col-span-4"
+                  onClick={() =>
+                    window.open(
+                      `${process.env.REACT_APP_URL}/event/${event.eventCategoryText}/${event.id}/${event.eventId}`
+                    )
+                  }
+                >
                   <div className="flex">
-                    <img src={EventCardImg} className='event-img' />
+                    <img src={EventCardImg} className="event-img" />
                   </div>
                   <div className="event-card-content">
                     <div className="flex items-center mb-0">
-                      <img src={event.eventCategoryImageUrl} className='event-icon' />
-                      <p className="event-title">
-                        {event.title}
-                      </p>
-                      <p className="event-fees">
-                        {`₹${event.fees}`}
-                      </p>
+                      <img
+                        src={event.eventCategoryImageUrl}
+                        className="event-icon"
+                      />
+                      <p className="event-title">{event.title}</p>
+                      <p className="event-fees">{`₹${event.fees}`}</p>
                     </div>
 
                     <div className="flex justify-between event-footer">
                       <p className="created-at"> {event.createdAtText} </p>
-                      <img src={ShareIcon} className='cursor-pointer' onClick={() => setShareModalActive(true)} />
+                      <img
+                        src={ShareIcon}
+                        className="cursor-pointer"
+                        onClick={() => setShareModalActive(true)}
+                      />
                     </div>
                     <div className="flex justify-between">
-                      <p className="event-venue">
-                        {event.venue}
-                      </p>
+                      <p className="event-venue">{event.venue}</p>
                       <p className="organizer">
                         organized by <span> {event.organisedBy.name} </span>
                       </p>
                     </div>
                   </div>
                 </div>
-              )
+              );
             })}
           </div>
         </div>
-
       </div>
 
-      <ShareEventModal show={shareModalActive}
-        handleClose={() => setShareModalActive(false)} />
-
+      <ShareEventModal
+        show={shareModalActive}
+        handleClose={() => setShareModalActive(false)}
+      />
     </>
   );
 };
